425900f86214c5e7d84703c22805a03302f3191c
Achraf <hardy1348@live.fr>
2021-01-13 12:18:42 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,91 @@
+// Exo1
+
+// let addition = (a,b) => {
+//     return `chiffre1 : ${a} + chiffre2 : ${b} = result : ${a + b}`
+// }
+// let chiffre1= addition(18,20)
+
+// console.log(chiffre1);
+
+// Exo2
+
+// let addition = (a,b) => {
+//     return `chiffre1 : ${a} - chiffre2 : ${b} = result : ${a - b}`
+// }
+// let chiffre1= addition(20,15)
+
+// console.log(chiffre1);
+
+// Exo3
+
+// let addition = (a,b) => {
+//     return `chiffre1 : ${a} x chiffre2 : ${b} = result : ${a * b}`
+// }
+// let chiffre1= addition(20,10)
+
+// console.log(chiffre1);
+
+// Exo4
+
+// let addition = (a,b) => {
+//     return `chiffre1 : ${a} / chiffre2 : ${b} = result : ${a / b}`
+// }
+// let chiffre1= addition(20,2)
+
+// console.log(chiffre1);
+
+// ExO5
+
+// let modulos = (a,b) => {
+//     let resultat = a % b
+//     return resultat
+// };
+
+// console.log(modulos(5,2));
+
+// Exo6
+
+// let racineCarre = (a) => {
+//     return (Math.sqrt(a))
+// }
+// console.log(racineCarre(120));
+
+// Exo7
+
+// let exposant = (a,b) => {
+//     return (Math.pow(a,b))
+// }
+// console.log(exposant(2,5));
+
+// Exo8
+
+// let capitalize = (s) => {
+//     if (s !== 'string')
+//     return s.charAt(0).toUpperCase() + s.slice(1)
+// }
+
+// console.log(capitalize('je suis doux'));
+
+// Exo9
+
+// let calcul = (nbr1,operator,nbr2) => {
+
+//     switch (operator) {
+//         case '+':
+//             return nbr1 + nbr2
+            
+//         case '-':
+//             return nbr1 - nbr2
+            
+//         case '/':
+//             return nbr1 / nbr2
+            
+//         case '*':
+//             return nbr1 * nbr2
+            
+//     }
+// }
+
+// console.log(calcul(5 ,'/', 10));
+
+
